732a831e3e5531fac7283a756227ac84a76fdb28
Luan Silva <luan.owsla@gmail.com>
2021-12-31 03:35:43 -0300

    desafio02: typed effect

--- desafio02-jordanshoes/src/components/Header/index.tsx
+++ desafio02-jordanshoes/src/components/Header/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import BlackBar from "../BlackBar";
 import style from "./header.module.scss";
 
+import Typed from "react-typed";
+
 const Header: React.FC = () => {
   return (
     <div className={style.Container}>
-      <BlackBar title="Ganhe R$ 10,00 de desconto no frete"/>
+      <BlackBar title="Ganhe R$ 10,00 de desconto no frete" />
 
       <div className={style.Brand}>
         <h1>JordanShoes</h1>
@@ -13,7 +15,12 @@ const Header: React.FC = () => {
 
       <div className={style.HeaderContent}>
         <h2>A melhor loja de Jordan</h2>
-        <p>O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.</p>
+        <Typed className={style.Text}
+          strings={[
+            "O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.",
+          ]}
+          typeSpeed={40}
+        />
       </div>
     </div>
   );
