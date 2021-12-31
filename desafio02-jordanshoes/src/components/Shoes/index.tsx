0830204c3640d598777ecc492dcc2dab5092c489
Luan Silva <luan.owsla@gmail.com>
2021-12-31 04:53:36 -0300

    desafio02: animation

--- desafio02-jordanshoes/src/components/Shoes/index.tsx
+++ desafio02-jordanshoes/src/components/Shoes/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
 
+import "aos/dist/aos.css";
 import style from "./shoes.module.scss";
 interface Props {
   url: string;
@@ -7,8 +9,11 @@ interface Props {
 }
 
 const Shoes: React.FC<Props> = ({ url, name }) => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
-    <div className={style.Card}>
+    <div data-aos="fade-up" data-aos-delay="200" className={style.Card}>
       <img src={url} alt="Jordan" />
       <span>{name}</span>
     </div>
