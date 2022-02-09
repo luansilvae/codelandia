514cc0da0d442d82624bac3f9314c9fe6ff10b15
Luan Silva <luan.owsla@gmail.com>
2022-02-09 18:36:28 -0300

    desafio05: add modal trailer

--- desafio05-studio-ghibli/src/pages/Home/index.tsx
+++ desafio05-studio-ghibli/src/pages/Home/index.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiPlay } from "react-icons/fi";
+import ModalVideo from "react-modal-video";
 
 import Header from "../../components/Header";
 import illustration from "../../image/ilustracao.png";
 
 import style from "./Home.module.scss";
+import "react-modal-video/scss/modal-video.scss";
 
 const Home: React.FC = () => {
+  const [isOpen, setOpen] = useState(false)
+  
   return (
     <main>
       <Header />
+      <ModalVideo
+        channel="youtube"
+        autoplay
+        isOpen={isOpen}
+        videoId="ByXuk9QqQkk"
+        onClose={() => setOpen(false)}
+      />
       <div className={style.Wrapper}>
         <div className={style.MainContent}>
           <img
@@ -29,13 +40,12 @@ const Home: React.FC = () => {
             </span>
 
             <div className={style.ButtonsBox}>
-              <a href="#">
+              <a href="https://www.netflix.com/br/title/60023642" target="_blank">
                 <span>
-                  {" "}
                   <FiPlay /> Assistir agora
                 </span>
               </a>
-              <a href="#">
+              <a href="#" onClick={()=> setOpen(true)}>
                 <span>Assista ao trailer</span>
               </a>
             </div>
